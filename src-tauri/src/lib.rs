f337f9eaa3f8984a72765acd5430fd29034a6806
fraasi <fraasi.gh@gmail.com>
2024-10-07 22:38:34 +0300

    display img from path & readDir

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri::Manager; // need for devtools?
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,6 +8,12 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            app.get_webview_window("main").unwrap().open_devtools(); // `main` is the first window from tauri.conf.json without an explicit label
+            Ok(())
+        })
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![greet])
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
   import { open } from "@tauri-apps/plugin-dialog";
-
+  import { appDataDir, join, homeDir } from "@tauri-apps/api/path";
+  import { convertFileSrc } from "@tauri-apps/api/core";
+  import  { readDir }  from '@tauri-apps/plugin-fs'
   let name = "";
   let greetMsg = "";
 
@@ -14,24 +16,52 @@
   // Open a dialog
   async function openDir() {
     try {
-
-      const file = await open({
+      const dir = await open({
         multiple: false,
         directory: true,
       });
-      console.log(file);
+      console.log(dir)
+      const files = await readDir(dir as string)
+      console.log(files.length)
+      for (let file of files) {
+        // check also if image
+        if (file.isFile) console.log(file.name)
+      }
+
+
       // Prints file path or URI
     } catch (e) {
       console.error(e);
     }
+  }
+  let imgUrl = "";
+  async function addUrl() {
+    // hack: Load small files via fs functions as binary
+    // https://github.com/tauri-apps/tauri/issues/3725#issuecomment-2147476960
+
+    const appDataDirPath = await appDataDir();
+    console.log("appDataDirPath", appDataDirPath);
+
+    // cant read from d, 'cos is in windows side?
+    // const filePath = await join('d', "Pics/2020-12-28_2143.png");
+    const filePath = await join(await homeDir(), 'Code/github/piksteri/static', 'svelte.svg')
+    console.log("filePath", filePath);
+
+    const u = convertFileSrc(filePath);
+    console.log("u", u);
+    imgUrl = u
+    // imgUrl = '/svelte.svg'
   }
 </script>
 
 <div class="container">
   <h1>Tauri!</h1>
 
-  <button type="button" on:click={openDir}>pen Directory</button>
-
+  <button type="button" on:click={openDir}>open Directory</button>
+  <button type="button" on:click={addUrl}>add image</button>
+  {#if imgUrl}
+    <img src={imgUrl} style="width: 100px" alt="pic" />
+  {/if}
   <div class="row">
     <a href="https://tauri.app" target="_blank">
       <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
@@ -52,7 +82,6 @@
 </div>
 
 <style>
-
   .logo.svelte-kit:hover {
     filter: drop-shadow(0 0 2em #ff3e00);
   }
