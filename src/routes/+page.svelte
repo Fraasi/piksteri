d21e53fc1be2f3c1ccc6a17eb782966c5ace7792
fraasi <fraasi.gh@gmail.com>
2024-10-10 18:58:03 +0300

    work on css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,121 +1,99 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
-  import { open } from "@tauri-apps/plugin-dialog";
-  import { appDataDir, join, homeDir } from "@tauri-apps/api/path";
-  import { convertFileSrc } from "@tauri-apps/api/core";
-  import { readDir } from "@tauri-apps/plugin-fs";
+  // import { invoke } from "@tauri-apps/api/core"
+  import { open } from "@tauri-apps/plugin-dialog"
+  import { join } from "@tauri-apps/api/path"
+  import { convertFileSrc } from "@tauri-apps/api/core"
+  import { readDir } from "@tauri-apps/plugin-fs"
 
-  let name = "";
-  let greetMsg = "";
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name });
-    console.log(greetMsg)
-  }
+  let numOfImages = 0
+  let folder = ""
+  let currentImgUrl = "/d/Pics/2020-12-28_2143.png"
+  let intervalId: number
 
   const extensions = new Set([
-	'bmp',
-	'djvu',
-	'gif',
-	'icns',
-	'ico',
-	'jpeg',
-	'jpg',
-	'pict',
-	'png',
-	'psd',
-	'raw',
-	'rgb',
-	'rgba',
-	'svg',
-	'tif',
-	'tiff',
-	'webp',
-]);
+    "bmp",
+    "djvu",
+    "gif",
+    "icns",
+    "ico",
+    "jpeg",
+    "jpg",
+    "pict",
+    "png",
+    "psd",
+    "raw",
+    "rgb",
+    "rgba",
+    "svg",
+    "tif",
+    "tiff",
+    "webp",
+  ])
 
-function isImage(filePath: string): boolean {
-	return extensions.has(filePath.split('.').at(-1)?.toLowerCase() as string);
-}
+  function isImage(filePath: string): boolean {
+    return extensions.has(filePath.split(".").at(-1)?.toLowerCase() as string)
+  }
 
-  // when using `"withGlobalTauri": true`, you may use: const { open } = window.__TAURI__.dialog;
+  // when using `"withGlobalTauri": true`, you may use: const { open } = window.__TAURI__.dialog
   // Open a dialog
   async function openDir() {
     try {
-      const dir = (await open({
+      folder = (await open({
         multiple: false,
         directory: true,
         // recursive: true, // https://github.com/tauri-apps/tauri/issues/4851
-      })) as string;
-      // console.log(dir);
-      const files = await readDir(dir);
+      })) as string
+      // console.log(dir)
+      const files = await readDir(folder)
       const images: string[] = []
 
       for (let file of files) {
         if (file.isFile && isImage(file.name)) {
-          const path = await join(dir, file.name)
+          const path = await join(folder, file.name)
           const pathConverted = convertFileSrc(path)
           images.push(pathConverted)
         }
-
-        let num = 0
-        setInterval(() => {
-          if (num >= images.length) {
-            num = 0
-          }
-          imgUrl = images[num]
-          num++
-        }, 5000)
       }
-      console.log('images in folder:', images.length)
 
-
-    } catch (e) { console.error(e) }
-  }
-  let imgUrl = "";
-  async function addUrl() {
-    // hack: Load small files via fs functions as binary
-    // https://github.com/tauri-apps/tauri/issues/3725#issuecomment-2147476960
-
-    const appDataDirPath = await appDataDir();
-    console.log("appDataDirPath", appDataDirPath);
-
-    // cant read from d, 'cos is in windows side?
-    // const filePath = await join('d', "Pics/2020-12-28_2143.png");
-    const filePath = await join(
-      await homeDir(),
-      "Code/github/piksteri/static",
-      "svelte.svg",
-    );
-    console.log("filePath", filePath);
-
-    const u = convertFileSrc(filePath);
-    console.log("u", u);
-    imgUrl = u;
-    // imgUrl = '/svelte.svg'
+      numOfImages = images.length
+      // start with random image & go in order from there
+      let num = Math.floor(Math.random() * numOfImages)
+      clearInterval(intervalId)
+      intervalId = setInterval(() => {
+        if (num >= numOfImages) num = 0
+        currentImgUrl = images[num]
+        console.log("image url:", currentImgUrl)
+        num++
+      }, 5000)
+    } catch (e) {
+      console.error(e)
+    }
   }
 </script>
 
 <div class="container">
-
   <button type="button" on:click={openDir}>open Directory</button>
-  <button type="button" on:click={addUrl}>add image</button>
-  {#if imgUrl}
-    <img src={imgUrl} style="width: 100px" alt="{imgUrl} file is not an image" />
+  {#if folder}
+    <p>Selected folder: {folder} - Number of images: {numOfImages}</p>
   {/if}
-
+  {#if currentImgUrl}
+    <img
+      id="current-img"
+      src={currentImgUrl}
+      alt="{currentImgUrl} file is not an image"
+    />
+  {/if}
 </div>
 
 <style>
-
   :root {
     font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
     font-size: 16px;
     line-height: 24px;
     font-weight: 400;
 
-    color: #0f0f0f;
-    background-color: #f6f6f6;
+    color: #f6f6f6;
+    background-color: #2f2f2f;
 
     font-synthesis: none;
     text-rendering: optimizeLegibility;
@@ -126,11 +104,8 @@ function isImage(filePath: string): boolean {
 
   .container {
     margin: 0;
-    padding-top: 10vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
+    width: 100vw;
+    height: 100vh;
   }
 
   button {
@@ -153,21 +128,13 @@ function isImage(filePath: string): boolean {
   }
   button:active {
     border-color: #396cd8;
-    background-color: #e8e8e8;
+    background-color: #0f0f0f69;
   }
 
-  @media (prefers-color-scheme: dark) {
-    :root {
-      color: #f6f6f6;
-      background-color: #2f2f2f;
-    }
-
-    button {
-      color: #ffffff;
-      background-color: #0f0f0f98;
-    }
-    button:active {
-      background-color: #0f0f0f69;
-    }
+  img#current-img {
+    /* display: block; */
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
   }
 </style>
